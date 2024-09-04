4921751bbeb6e1250b892e3540786f51212415d1
anshudubey344 <95116627+anshudubey344@users.noreply.github.com>
2024-09-04 22:02:56 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-# Define the port and process name to be stopped
-PORT=8001
-PROCESS_NAME="node"
-
-# Find and kill the process listening on the specified port
-PID=$(lsof -t -i :$PORT)
-
-if [ -n "$PID" ]; then
-    echo "Stopping existing Node.js application with PID $PID"
-    kill -9 $PID
-else
-    echo "No Node.js application is running on port $PORT"
-fi
+# Stop the Node.js application
+pm2 stop todo-app || true
+pm2 delete todo-app || true
