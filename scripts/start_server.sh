2a20d719dea4f781490fcb36023cb70d12080e16
anshudubey344 <95116627+anshudubey344@users.noreply.github.com>
2024-09-04 19:53:11 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/todo-cicd
-sudo npm start > app.out.log 2> app.err.log < /dev/null &
-
+sudo npm install
+sudo npm install -g pm2
+sudo pm2 start view/todo.ejs -f --name api
